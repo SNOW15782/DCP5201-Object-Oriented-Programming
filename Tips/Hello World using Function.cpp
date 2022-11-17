0d34f509bf52b9e63466f8aad8865977db402a72
Arif <111127538+SNOW15782@users.noreply.github.com>
2022-11-17 22:44:39 +0800

    Add files via upload

---
+++ Tips/Hello World using Function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+// declare
+void helloworld()
+{
+	cout << "Hello world!"<<endl;
+}
+
+//callback 
+int main ()
+{
+	helloworld();
+	helloworld();
+	helloworld();
+	return 0; 
+}
+
+//no defition required as declare already do the calculation 
