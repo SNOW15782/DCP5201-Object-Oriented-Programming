c697db503926cabe3916cc68f1da44f7362d856a
Arif <111127538+SNOW15782@users.noreply.github.com>
2022-12-05 23:08:37 +0800

    Add files via upload

---
+++ Tips/For Loop Array for Rectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int f;
+	int rectangle [f];
+
+	cout << "How many rectangle? : ";
+	cin >> f;
+	
+	for (int i = 0; i <f ; i++)
+	{
+	 cout << "\nrectangle "<<1+i <<"\n";
+	
+	}
+	
+	return 0; 
+}
