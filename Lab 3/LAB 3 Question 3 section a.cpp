6bb945d39fcdaac7b0a650739172df2560876420
Arif <111127538+SNOW15782@users.noreply.github.com>
2024-05-07 14:53:54 +0800

    Update LAB 3 Question 3 section a.cpp

--- Lab 3/LAB 3 Question 3 section a.cpp
+++ Lab 3/LAB 3 Question 3 section a.cpp
@@ -3,83 +3,73 @@
 //QUESTION 3
 //SECTION A
 
-#include<iostream>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
 using namespace std;
 
-class Purchase
-{ 
-private: 
-char name [50];
-char code [50];
-int qty;
-float price; 
-float total; 
-
+class Purchase {
+private:
+    char name[50];
+    char code[10];
+    int qty;
+    float price;
+    float total;
 public:
-
-//Set all the data to the appropriate variable
-//Display the key in here 
-void set_data()
-{
-//Enter name 
-cout << "Enter Name	: ";
-cin >> name; 
-
-//Enter product code 
-cout << "Product Code	: ";
-cin >> code; 
-
-//Enter quantity
-cout << "Enter Quantity	: ";
-cin >> qty;
-
-//Enter price 
-cout << "Enter Price	: RM ";
-cin >> price; 
-	
-}
-
-//Calculate the total amount of payment to be made 
-void calculate ()
-{
-	total = qty * price;
-}
-
-//PRINT FOR RECEIPT SECTION 
-void print ()
-{
-cout << "========================\n";
-cout << "RECEIPT\n";
-cout << "========================\n";
-
-//NAME CALL
-cout << "Name		: "<< name<< endl; 
-
-//PRODUCT CODE CALL
-cout << "Product Code    : "<<code<< endl; 
-
-//QUANTITY CALL 
-cout << "Quantity	: "<<qty<< endl; 
-
-//PRODUCT PRICE CALL 
-cout << "Product Price	: RM "<<price<< endl;
- 
-//TOTAL PRICE CALL 
-cout << "Total Payment	: RM "<<total<< endl; 
-}
-
+    void set_data(const char* n, const char* c, int q, float p);
+    void calculate();
+    void print();
 };
 
-int main()
-
-{ 
-Purchase purchase;
-cout << "========================" <<endl;
-cout << "	WELCOME" << endl;
-cout << "========================" <<endl;
-
-purchase.set_data();
-purchase.calculate();
-purchase.print();
-return 0;
+void Purchase::set_data(const char* n, const char* c, int q, float p) {
+    strcpy(name, n);
+    strcpy(code, c);
+    qty = q;
+    price = p;
 }
+
+void Purchase::calculate() {
+    total = qty * price;
+}
+
+void Purchase::print() {
+    cout << "========================" << endl;
+    cout << "RECEIPT" << endl;
+    cout << "========================" << endl;
+    cout << "Name: " << name << endl;
+    cout << "Product Code: " << code << endl;
+    cout << "Quantity: " << qty << endl;
+    cout << "Product Price: RM " << fixed << setprecision(2) << price << endl;
+    cout << "Total Payment: RM " << fixed << setprecision(2) << total << endl;
+}
+
+int main() {
+    cout << "========================" << endl;
+    cout << "WELCOME" << endl;
+    cout << "========================" << endl;
+
+    Purchase p;
+    char name[50];
+    char code[10];
+    int qty;
+    float price;
+
+    cout << "Enter name: ";
+    cin.getline(name, 50);
+
+    cout << "Product code: ";
+    cin.getline(code, 10);
+
+    cout << "Enter quantity: ";
+    cin >> qty;
+
+    cout << "Enter price: RM ";
+    cin >> price;
+
+    p.set_data(name, code, qty, price);
+    p.calculate();
+    p.print();
+
+    return 0;
+}
+
