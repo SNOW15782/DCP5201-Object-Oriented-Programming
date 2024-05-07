a9f3ead7110bdce3e32ce583a4ab48e88da60a0d
Arif <111127538+SNOW15782@users.noreply.github.com>
2024-05-07 22:15:56 +0800

    Update Lab 3 Q4.cpp

--- Lab 3/Lab 3 Q4.cpp
+++ Lab 3/Lab 3 Q4.cpp
@@ -2,74 +2,77 @@
 //LAB 3 
 //Q4 
 
-#include <iostream>
-using namespace std; 
-
-//global size = 8 
+#include<iostream>
+ 
+using namespace std;
+ 
+//constant global variable
 const int SIZE = 8;
-
-class Stationery_Inventory
-{
-	//private member 
-	private: 
-	string code;
-	int warehouses [SIZE]; 
-	
-	//public member 
-	public: 
-	
+ 
+//define class object
+class Stationary_Inventory
+{	//define private data members
+	private:
+		string name;
+		int warehouses[SIZE];
+	//define public member functions
+	public:
+		//display array elements in reverse order using looping structure
 		void display_reverse()
 		{
-			//looping structure 
-			//decrement 
-			//display the array elements of warehouses in reverse order using any looping strucutre 
-			for (int i = SIZE - 1; i>= 0 ; i--) 
-			{
-				cout << warehouses[i] << " "<<endl; 
-			}
+			int i;
+		    int j=0;
+		    cout<<"\n-------------------------------"<<endl;
+		    cout<<"       The Inventory Info        "<<endl;
+		    cout<<"---------------------------------"<<endl;
+		    cout<<"Stationery code :"<<name<<endl;
+		    //decremental indexing to display last data in warehouse array first
+		    for(i=SIZE-1; i>=0; i--)
+	        	cout<<"Warehouse "<<++j<<"\t:"<<warehouses[i]<<endl;
 		}
-	//function has a pointer argument 
-	void set_data (int* arr)
-	{
-		//get user input for code name 
-		cout << "Enter Stationery Code: ";
-		cin >> code; 
-		for (int i = 0; i< SIZE; i++)
+		//setting data of private data members with pointer argument
+		void set_data(int *p)
 		{
-			//use the pointer argument to initialize warehouses array
-			warehouses[i] = *(arr+i); 
+			int i;
+		    cout<<"\n######################################"<<endl;
+		    cout<<"         Item Entry                   "<<endl;
+		    cout<<"--------------------------------------"<<endl;
+		    //get user input for code name
+		    cout<<"Enter Stationery code :";
+		    cin>>name;
+		    //for loop to initialize warehouses array with pointer input
+		    for(i=0; i<SIZE; i++) 
+          		warehouses[i] = *(p+i);
+ 
 		}
-	}
-
-	
-}; //global object delcarion, hold 
-Stationery_Inventory hold; 
-
-void process ()
+} hold; //declare global object
+ 
+//define process function
+void process()
 {
-	int arr [SIZE];
-	//gt user input for 8 values that should stored in a local array 
-	cout << "Enter the stocks for 8 warehouses: "; 
-	for (int i = 0; i < SIZE; i++)
-	{
-	cin >> arr[i]; 	
+	cout<<"      Colourful Book Holder Stock     "<<endl;
+  	cout<<"--------------------------------------"<<endl;
+  	//get user to input 8 values to be stored in local array
+  	cout<<"Enter the stocks for 8 warehouses   :"<<endl;
+  	int arr[SIZE]; //initialize local array
+  	for(int i=0; i<SIZE; i++)
+	{ 
+		cin>>arr[i];
 	}
-	//using global object hold, call function set_data(...), passing the array and call display_reverse()
-	hold.set_data (arr); 
+	//call member functions of global object
+	hold.set_data(arr);
 	hold.display_reverse();
-	
 }
-
-int main ()
-{
-	//declare an object at class above 
-	Stationery_Inventory obj; 
-	int arr [SIZE] = {5,10,15,22,20,25,30,35};
-	obj.set_data (arr);
-	obj.display_reverse();
-	
-	//call process 
-	process (); 
-	
-	return 0; 
+ 
+int main()
+{	//declare object of class
+	Stationary_Inventory Stat;
+	//declare array of 8 integer elements and initialize
+	int array[SIZE] = {5,10,15,22,20,25,30,35};
+	//set data of object using array by calling member function
+	Stat.set_data(array);
+	//call object member function to display data in reverse
+	Stat.display_reverse();
+	//call process function to set and display data of global object
+	process();
 }
